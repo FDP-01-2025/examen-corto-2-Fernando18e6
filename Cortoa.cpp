f4fb56b007a630aee583d0139d0d52c1355e4d8c
Fernando18e6 <00137925@uca.edu.sv>
2025-05-13 10:48:33 -0600

    Corto A

--- Cortoa.cpp
+++ Cortoa.cpp
@@ -3,14 +3,12 @@
 
 using namespace std;
 
-main()
-{
-    // Se declaran las variables
-    int zapatos, genero, dinero=0, edad=0, compra, zapCM=1, zapEM=1, zapPM=1, zapDH=64, zapFH=96, zapPH=200;
+int main()
+{ // Se declaran las variables
+    int zapatos, genero, dinero = 0, edad = 0, compra, zapCM = 0, zapEM = 0, zapPM = 0, zapDH = 64, zapFH = 96, zapPH = 200;
 
-
-zapCM=0.2 * 80;
-cout<<zapCM;
+    zapCM = 0.2 * 80;
+    cout << zapCM;
 
     // Se pide una cantidad de dinero.
     cout << "Ingrese su cantidad de dinero";
@@ -19,7 +17,7 @@ cout<<zapCM;
     // Se pide al usuario que nos de su edad
     cout << "Ingrese su edad ";
     cin >> edad;
-    //Se verifica la edad y se asigan una talla
+    // Se verifica la edad y se asigan una talla
     if (edad > 0 && edad < 12)
     {
         cout << "Su talla es 34 ";
@@ -37,114 +35,119 @@ cout<<zapCM;
     {
         cout << "Su talla es de 42 ";
     }
-    else
-
-    cout << "Ahora ayudenos ingresando su genero\n ";
+    else{
+        //Se pide el genero
+        cout << "Ahora ayudenos ingresando su genero\n ";
     cout << "1. Mujer\n ";
     cout << "2. Hombre\n";
     cout << "3. Otros\n ";
     cin >> genero;
-
-        switch(genero){
-            case 1:{ 
-                cout << "Gracias el sistema te reconoce como mujer, tenemos ofertas solo para ti\n";
-                zapCM= 80/200;
-                cout << "1. Zapato casual en \n"<<zapCM;
-                cout << "2. Zapato elegante \n"<<zapEM;
-                cout << "3. Zapato premium \n"<<zapPM;
-                cout << "Porvaor ingrese cual zapato desea ";
-                cin>>zapatos;
-                
-
-                switch(zapatos){
-                    case 1:
-                    if (dinero<zapCM){
-                        cout<<"Su monto es insuficiente";
-
-                    }
-                    else 
-                        dinero= zapCM - dinero;
-                        cout << "Su compra se realizo con exito, su saldo es de "<<dinero;
-                    break;
-
-                    case 2: 
-                    if (dinero<zapEM){
-                        cout << "Su monto es insuficiente ";
-
-                    }
-                    else
-
-                    dinero= zapEM - dinero;
-                        cout << "Su compra se realizo con exito, su saldo es de "<<dinero;
-                    break;
-                
-                    case 3:
-                    if (dinero<zapPM){
-                        cout << "Su monto es insuficiente ";
-                    }
-                    else
-                    dinero= zapPM- dinero; 
-                        cout << "Su compra se realizo con exito, su saldo es de "<<dinero;
-                    break;
-
-                    default:
-                    cout << "Ingrese un formato valido ";
-                    
-
-
-
-
-
-                }
-
-                case 2:
-                cout << "Gracias el sistema te reconoce como mujer, tenemos ofertas solo para ti\n";
-                cout << "1. Zapato casual en \n"<<zapDH;
-                cout << "2. Zapato elegante \n"<<zapFH;
-                cout << "3. Zapato premium \n"<<zapPH;
-                cout << "Porvaor ingrese cual zapato desea ";
-                cin>>zapatos;
-                
-
-                switch(zapatos){
-                    case 1:
-                    if (dinero<zapDH){
-                        cout<<"Su monto es insuficiente";
-
-                    }
-                    else{
-                        dinero= zapFH - dinero;
-                        cout << "Su compra se realizo con exito, su saldo es de "<<dinero;
-                    }
-                    break;
-
-                    case 2: 
-                    if (dinero<zapEM){
-                        cout << "Su monto es insuficiente ";
-
-                    }
-                    else{
-
-                    dinero= zapEM - dinero;
-                        cout << "Su compra se realizo con exito, su saldo es de "<<dinero;
-                    }
-                    break;
-                
-                    case 3:
-                    if (dinero<zapPH){
-                        cout << "Su monto es insuficiente ";
-                    }
-                    else
-                    dinero= zapPH- dinero; 
-                        cout << "Su compra se realizo con exito, su saldo es de "<<dinero;
-                    break;
-
-                    default:
-                    cout << "Ingrese un formato valido ";
-                    
-
+    }
+    //Se analiza cada caso siendo mujer 
+    switch (genero)
+    {
+    case 1:
+        cout << "Gracias el sistema te reconoce como mujer, tenemos ofertas solo para ti\n";
+        zapCM = 80 / 200;
+        cout << "1. Zapato casual en \n"
+             << zapCM;
+        cout << "2. Zapato elegante \n"
+             << zapEM;
+        cout << "3. Zapato premium \n"
+             << zapPM;
+        cout << "Porvaor ingrese cual zapato desea ";
+        cin >> zapatos;
+        //Se evaluan los zapatos
+        switch (zapatos)
+        {
+            case 1:
+            if (dinero < zapCM)
+            {
+                cout << "Su monto es insuficiente";
             }
+
+            zapCM = 70 * 0.15;
+            dinero = zapCM - dinero;
+            cout << "Su compra se realizo con exito, su saldo es de " << dinero;
+            break;
+
+            case 2:
+            if (dinero < zapEM)
+            {
+                cout << "Su monto es insuficiente ";
+            }
+            else
+                dinero = 100 * 0.15;
+            dinero = zapEM - dinero;
+            cout << "Su compra se realizo con exito, su saldo es de " << dinero;
+            break;
+
+            case 3:
+            if (dinero < zapPM)
+            {
+                cout << "Su monto es insuficiente ";
+            }
+            else{
+                zapPM=200 * 0.15;
+                dinero = zapPM - dinero;
+            cout << "Su compra se realizo con exito, su saldo es de " << dinero;
+            }
+            break;
+
+            default:
+            cout << "Ingrese un formato valido ";
         }
 
-    return 0;
+    case 2:
+            cout << "Gracias el sistema te reconoce como mujer, tenemos ofertas solo para ti\n";
+            cout << "1. deportivo en \n"
+             << zapDH;
+            cout << "2. Zapato Formal \n"
+             << zapFH;
+            cout << "3. Zapato premium \n"
+             << zapPH;
+            cout << "Porvaor ingrese cual zapato desea ";
+            cin >> zapatos;
+            //Se evalua cada caso siendo hombre
+            switch (zapatos)
+            {
+            case 1:
+            if (dinero < zapDH)
+            {
+                cout << "Su monto es insuficiente";
+            }
+            else
+            {
+                dinero = zapFH - dinero;
+                cout << "Su compra se realizo con exito, su saldo es de " << dinero;
+            }
+            break;
+
+            case 2:
+            if (dinero < zapEM)
+            {
+                cout << "Su monto es insuficiente ";
+            }
+            else
+            {
+
+                dinero = zapEM - dinero;
+                cout << "Su compra se realizo con exito, su saldo es de " << dinero;
+            }
+            break;
+
+            case 3:
+            if (dinero < zapPH)
+            {
+                cout << "Su monto es insuficiente ";
+            }
+            else
+                dinero = zapPH - dinero;
+            cout << "Su compra se realizo con exito, su saldo es de " << dinero;
+            break;
+
+            default:
+            cout << "Ingrese un formato valido ";
+        }
+    }
 }
